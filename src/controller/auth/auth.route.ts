5a4bc4e340526dc444b223915154b83a01fc9120
jhon <jhon041413@gmail.com>
2024-08-07 14:49:32 +0800

    wip

--- src/controller/auth/auth.route.ts
+++ src/controller/auth/auth.route.ts
@@ -9,8 +9,8 @@ const router = express.Router();
 router.post("/login",processRequestBody(loginSchema.body),loginHander)
 
 router.get('/logout', (req, res) => {
-    res.cookie('refreshToken', { path: '/', domain: '.onrender.com',secure: true, httpOnly: true,sameSite:"none",expires:new Date()});
-    res.cookie('accessToken', { path: '/', domain: '.onrender.com',secure: true, httpOnly: true,sameSite:"none" ,expires:new Date() });
+    res.cookie('refreshToken', { path: '/', domain: 'docs-api-9r6n.onrender.com',secure: true, httpOnly: true,sameSite:"none",expires:new Date()});
+    res.cookie('accessToken', { path: '/', domain: 'docs-api-9r6n.onrender.com',secure: true, httpOnly: true,sameSite:"none" ,expires:new Date() });
 
     res.clearCookie('refreshToken', { path: '/', domain: 'localhost',secure: true, httpOnly: true,sameSite:"none" });
     res.clearCookie('accessToken', { path: '/', domain: 'localhost' ,secure: true, httpOnly: true,sameSite:"none" });
