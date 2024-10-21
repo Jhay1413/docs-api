13e07606208477c1f8e90ef6ece65a8b9ca5e7a0
Andre <jaguardian00@gmail.com>
2024-10-21 09:42:12 +0800

    wip

--- src/controller/ticketing/ticketing.routes.ts
+++ src/controller/ticketing/ticketing.routes.ts
@@ -5,8 +5,8 @@ const router = Router();
 const ticketingController = new TicketingController();
 
 // route for creating a new ticket
-router.post('/create/tickets', (req, res) => ticketingController.createTicket(req, res));
-router.get('/tickets/list', (req, res) => ticketingController.fetchTickets(req, res));
+router.post('/tickets/create', (req, res) => ticketingController.createTicket(req, res));
+router.get('/tickets', (req, res) => ticketingController.fetchTickets(req, res));
 router.put('/tickets/edit/:ticketId', (req, res) => ticketingController.updateTicket(req, res));
 
 export default router;
