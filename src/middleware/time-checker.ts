defc4abff040c9c63f20c7784ada7eb249aec547
Jhon <jhon041413@gmail.com>
2024-10-21 09:54:00 +0800

    wip

--- src/middleware/time-checker.ts
+++ src/middleware/time-checker.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 const disableAfter5PM = (req: Request, res: Response, next: NextFunction) => {
   const now = new Date();
   const currentHour = now.getHours();
-
+  console.log(currentHour);
   if (currentHour >= 8) {
     return res.status(403).json({ message: "Access Denied. " });
   }
